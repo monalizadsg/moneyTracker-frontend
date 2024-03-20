2b9b7971fec1b7cbd509a40075bec2b59c1257f5
Mona Dasig <monalizadasig@gmail.com>
2024-03-20 15:18:57 -0700

    Setup vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+  },
+});
+
+createApp(App).use(vuetify).mount("#app");
