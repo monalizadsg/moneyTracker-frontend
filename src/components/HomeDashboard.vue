b719ca382917c6f3e0431f97dba7cf9ffbf60166
Mona Dasig <monalizadasig@gmail.com>
2024-03-22 16:18:12 -0700

    Add initial components for routing

---
+++ src/components/HomeDashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <AppBar />
+    <div class="main-content">Home</div>
+  </div>
+</template>
+
+<script>
+import AppBar from "./AppBar.vue";
+
+export default {
+  name: "HomeDashboard",
+  components: {
+    AppBar,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+}
+
+.main-content {
+  /* border: 1px solid green; */
+  height: calc(100vh - 76px);
+}
+</style>
