3ab709c310bd690baa2f3b8e0b6dcacc1f89e765
Mirna Garcia <mhannany@gmail.com>
2024-04-01 14:15:33 -0700

    Goals-dashboard display goals list

--- src/components/GoalDashboard.vue
+++ src/components/GoalDashboard.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="container">
     <AppBar />
-    <div class="main-content">Goal</div>
+    <div class="main-content">
+      <div class="header">
+        <h4>Goals</h4>
+           <form-dialog :title="dialogTitle">
+              <v-form>
+                 <v-text-field
+                    v-model="formData.description"
+                    label="Description"
+                    variant="outlined"
+                 ></v-text-field>
+                 <v-text-field
+                    v-model="formData.targetAmount"
+                    label="Goal amount"
+                    variant="outlined"
+                 ></v-text-field>
+                    <v-text-field
+                    v-model="formData.currentAmount"
+                    label="Current saved amount"
+                    variant="outlined"
+                 ></v-text-field>
+                <v-text-field
+                    v-model="formData.startDate"
+                    label="Start Date"
+                    variant="outlined"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="formData.targetDate"
+                    label="Target Date"
+                    variant="outlined"
+                  ></v-text-field>
+            </v-form>
+          </form-dialog>
+      </div>
+      <GoalsList></GoalsList>
+    </div>
   </div>
 </template>
 
 <script>
 import AppBar from "./AppBar.vue";
+import FormDialog from "./FormDialog.vue";
+import GoalsList from "./GoalsList.vue";
 
 export default {
-  name: "GoalDashboard",
-  components: {
+   name: "GoalDashboard",
+    components: {
     AppBar,
+    FormDialog,
+    GoalsList
   },
+  data(){
+    return{
+      dialogTitle: "Add Goal",
+      formData: {
+        description: "",
+        targetAmount: "",
+        currentAmount: "",
+        startDate: "",
+        targetDate: "",
+      },
+    }
+
+  },
+
 };
 </script>
 
@@ -24,5 +76,13 @@ export default {
 .main-content {
   /* border: 1px solid green; */
   height: calc(100vh - 76px);
+  padding: 20px;
+}
+
+.header {
+  display: flex;
+  /* border: 1px solid green; */
+  justify-content: space-between;
+   padding: 20px;
 }
 </style>
